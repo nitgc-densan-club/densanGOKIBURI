31a57679a5838cd873ebff37a2a17c049af1fae5
KKAZU3 <kazu256think@outlook.jp>
2019-12-17 18:06:13 +0900

    Made PlayerControl

---
+++ Assets/Add.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Add : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+      float speed = 1.3f;
+      Rigidbody rb ;
+    float x;
+    float z;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        /*
+        x = Input.GetAxis("Horizontal") * speed;
+        z = Input.GetAxis("Vertical") * speed;
+        rb.AddForce(x, 0, z);
+        */
+    }
+    void Update()
+    {
+        x = Input.GetAxisRaw("Horizontal") * speed;
+        z = Input.GetAxisRaw("Vertical") * speed;
+        rb.AddForce(new Vector3(0, 0, z), ForceMode.Impulse);
+    }
+}
+
+    
---
+++ Assets/Track.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Track : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+    public GameObject target;
+    NavMeshAgent agent;
+    // Use this for initialization
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = target.transform.position;
+    }
+}
