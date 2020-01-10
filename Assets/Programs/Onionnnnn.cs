0827f04f860afc814dcf7d9949760a2f0360dfd1
o0Latimeria0o <397oom@gmail.com>
2020-01-10 18:04:27 +0900

    Edit a script.

--- Assets/Programs/Onionnnnn.cs
+++ Assets/Programs/Onionnnnn.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class Onion : MonoBehaviour
 {
+    public Rigidbody rby;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rby = GetComponent<Rigidbody>();
+        rby.AddForce()
     }
-
     // Update is called once per frame
     void Update()
     {
