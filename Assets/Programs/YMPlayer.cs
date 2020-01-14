8c5eb3fe8cef079cebab731cbce945031da00aa7
o0Latimeria0o <397oom@gmail.com>
2020-01-14 17:31:09 +0900

    edit a script

---
+++ Assets/Programs/YMPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YMPlayer : MonoBehaviour
+{
+
+  /*  float X;
+    float Z;
+    float sp;
+    public Rigidbody rby;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rby = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        X = Input.GetAxisRaw("Horizontal") * sp;
+        Z = Input.GetAxisRaw("Vertical") * sp;
+        rby.AddForce(X, 0, Z, ForceMode.Impulse);
+        
+    }*/
+}
