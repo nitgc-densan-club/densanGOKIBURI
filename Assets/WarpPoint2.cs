e7d020ccdd4781e97865b048ee2d4ef52a6e2a10
うなぎ <chika1126@gmail.com>
2020-01-17 18:14:47 +0900

    debug room 2nd made sroup made oilSample made tried to make warp script but cannot

---
+++ Assets/WarpPoint2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarpPoint2: MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.transform.position = new Vector3(6,0,20);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
