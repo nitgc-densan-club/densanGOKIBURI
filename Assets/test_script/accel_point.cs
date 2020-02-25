ed920f7b3262cd54048c14fe3aeff549ed736c0b
うなぎ <chika1126@gmail.com>
2020-02-25 18:24:27 +0900

    reset kitchen. make script of accel floor(test)

--- Assets/test_script/accel_point.cs
+++ Assets/test_script/accel_point.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class accel_point : MonoBehaviour
 {
+    public float speedX; //ｙ方向の速度
+    public float speedZ;// ｚ方向の速度
+    public float minSpeed　= 1;//最小速度
+    public float accel = 1; //加速度
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +22,16 @@ public class accel_point : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        other.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 10, 30), ForceMode.VelocityChange);
+        if(minSpeed <= speedX)//x方向の最低加速度
+        {
+            speedX = speedX - accel;
+            other.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(speedX, 0, 0));//x方向に加速
+        }
+        if (minSpeed <= speedZ)
+        {
+            speedZ = speedZ - accel;
+            other.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, speedZ));//z方向に加速
+        }
+        
     }
 }
